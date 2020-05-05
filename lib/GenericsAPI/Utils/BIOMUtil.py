7be20ab64c32411feac29f593519d37d3f89215e
Tianhao-Gu <tgu@anl.gov>
2020-05-05 18:15:36 -0500

    optional lineage

--- lib/GenericsAPI/Utils/BIOMUtil.py
+++ lib/GenericsAPI/Utils/BIOMUtil.py
@@ -257,7 +257,8 @@ class BiomUtil:
                 delimiter = csv.Sniffer().sniff(lineage).delimiter
                 lineage = [x.strip() for x in lineage.split(delimiter)]
 
-            taxonomy['lineage'] = lineage
+            if lineage:
+                taxonomy['lineage'] = lineage
 
             for key in ['score', 'taxonomy_source', 'species_name']:
                 val = self._retrieve_value([], datarow, key)
