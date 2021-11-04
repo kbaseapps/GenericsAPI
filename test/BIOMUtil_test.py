7bc826ebfedf6e4e4e64397d82b7c09c263c0721
Tianhao-Gu <tgu@anl.gov>
2021-11-03 23:33:48 -0500

    fix tests

--- test/BIOMUtil_test.py
+++ test/BIOMUtil_test.py
@@ -639,8 +639,7 @@ class BioMultiTest(unittest.TestCase):
                       }
             self.getImpl().import_matrix_from_biom(self.ctx, params)[0]
 
-    @patch.object(DataFileUtil, "file_to_shock", side_effect=mock_file_to_shock)
-    def test_fetch_sequence(self, file_to_shock):
+    def test_fetch_sequence(self):
         self.start_test()
 
         fake_object_ref = self.createAnObject()
